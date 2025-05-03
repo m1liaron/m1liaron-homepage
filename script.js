64c3e2bb3e29f09b12186235b8f622a41ba92913
machinker34@gmail.com <amex7708@gmail.com>
2025-05-03 23:32:46 +0300

    feat: additional programming skils

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Skills animation
     function animateSkillBars() {
-      document.getElementById('html-progress').style.width = '60%';
-      document.getElementById('css-progress').style.width = '65%';
-      document.getElementById('js-progress').style.width = '40%';
-      document.getElementById('react-progress').style.width = '50%';
-      document.getElementById('nodejs-progress').style.width = '50%';
+      document.getElementById('html-progress').style.width = '70%';
+      document.getElementById('css-progress').style.width = '75%';
+      document.getElementById('js-progress').style.width = '50%';
+      document.getElementById('react-progress').style.width = '60%';
+      document.getElementById('nodejs-progress').style.width = '60%';
     }
 
     animateSkillBars();
