f9408dc3b1286992aa4de836951547cb1b661f0a
machinker34@gmail.com <amex7708@gmail.com>
2025-05-04 01:00:46 +0300

    feat: projects show in iframe

--- script.js
+++ script.js
@@ -8,5 +8,36 @@ document.addEventListener('DOMContentLoaded', function() {
       document.getElementById('nodejs-progress').style.width = '60%';
     }
 
+     // Projects modals
+
+     const projects = document.querySelectorAll('.project');
+     const modal = document.getElementById('modal');
+     const siteFrame = document.getElementById('site-frame');
+     const projectName = document.getElementById('project-name')
+     const projectUrl = document.getElementById('project-url')
+     const closeButton = document.querySelector('.close-button');
+ 
+     projects.forEach(project => {
+       project.addEventListener('click', () => {
+         const url = project.getAttribute('data-url');
+         siteFrame.src = url;
+         modal.style.display = "block"
+         projectName.innerText = project.textContent;
+         projectUrl.href = url
+       })
+     });
+ 
+     closeButton.addEventListener("click", () => {
+       modal.style.display = "none";
+       siteFrame.src = "";
+     })
+ 
+     window.addEventListener("click", (event) => {
+       if(event.target == modal) {
+         modal.style.display = "none";
+         siteFrame.src = "";
+       }
+     })
+
     animateSkillBars();
   });
